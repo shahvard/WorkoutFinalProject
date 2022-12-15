e9ab0f63673c22e0aaa01322a2e626e11e9df6ec
shahvard <shahvard@sheridancollege.ca>
2022-12-14 22:05:45 -0500

    navigation drawer added

--- app/src/main/java/project/st991548772/vardhmanchandan/Diet/DietFragment.kt
+++ app/src/main/java/project/st991548772/vardhmanchandan/Diet/DietFragment.kt
@@ -24,24 +24,20 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import project.st991548772.vardhmanchandan.R
-import project.st991548772.vardhmanchandan.databinding.FragmentDietBinding
+import project.st991548772.vardhmanchandan.databinding.FragmentDiet2Binding
+
 import java.util.*
 
 
 class DietFragment : Fragment() {
 
-    private lateinit var binding: FragmentDietBinding
-  /*  private lateinit var yearSpinner: Spinner
-    private lateinit var monthSpinner: Spinner*/
+    private lateinit var binding: FragmentDiet2Binding
     private lateinit var email: String
     private lateinit var rView: RecyclerView
     private lateinit var auth: FirebaseAuth
     private val viewModel by lazy {
         ViewModelProvider(this).get(DietViewModel::class.java)
     }
-
-
-
 
     lateinit var edt : EditText
     var myDate = ""
@@ -51,15 +47,13 @@ class DietFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = DataBindingUtil.setContentView(this.requireActivity(), R.layout.fragment_diet)
-       /* yearSpinner = binding.year
-        monthSpinner = binding.month*/
+        //setting the lateinit variables
+        binding = DataBindingUtil.setContentView(this.requireActivity(), R.layout.fragment_diet2)
         auth = Firebase.auth
         rView = binding.list
-
         email = auth.currentUser?.email.toString()
-        val c = Calendar.getInstance()
 
+        val c = Calendar.getInstance()
         // on below line we are getting
         // our day, month and year.
         val year = c.get(Calendar.YEAR)
@@ -67,10 +61,12 @@ class DietFragment : Fragment() {
         val day = c.get(Calendar.DAY_OF_MONTH)
         val dat = (day.toString() + "-" + (month + 1) + "-" + year)
 
+        //calling the viewmodel to display today's date records
         viewModel.getFromDatabase(dat,email)
 
         edt = binding.date
 
+        //when the edittext is clicked an popup calendar is displayed
         edt.setOnClickListener {
             val c = Calendar.getInstance()
 
@@ -103,21 +99,24 @@ class DietFragment : Fragment() {
         }
 
 
+        //navigating to the add diet fragment
         binding.add.setOnClickListener(){
-            Navigation.findNavController(this.requireView()).navigate(R.id.action_dietFragment_to_addDietFragment)
+            Navigation.findNavController(this.requireView()).navigate(R.id.action_dietFragment2_to_addDietFragment)
         }
 
-
+        //observing the dietList in viewmodel if there is change display method will be called
         viewModel.dietList.observe(viewLifecycleOwner, androidx.lifecycle.Observer {
             display(it)
 
         })
 
+        //observing the totalCalories in viewmodel if there is change display method will be called
         viewModel.totalCalories.observe(viewLifecycleOwner, androidx.lifecycle.Observer {
             displayCalories()
 
         })
 
+        //observing the todaysCalories in viewmodel if there is change display method will be called
         viewModel.todaysCalories.observe(viewLifecycleOwner, androidx.lifecycle.Observer {
             displayCalories()
 
@@ -126,41 +125,24 @@ class DietFragment : Fragment() {
         val appCompat = requireActivity() as AppCompatActivity
         val navHostFragment = appCompat.supportFragmentManager.findFragmentById(R.id.studentNavHost) as NavHostFragment
         val navController = navHostFragment.findNavController()
-
         binding.bottomNav.setupWithNavController(navController)
 
-        binding.goHome.setOnClickListener(){
-            Navigation.findNavController(this.requireView()).navigate(R.id.action_dietFragment_to_homeFragment)
-        }
-
-
-
-        return inflater.inflate(R.layout.fragment_diet, container, false)
+        return inflater.inflate(R.layout.fragment_diet2, container, false)
 
 
     }
 
+    //displaying the calories on the textview
     private fun displayCalories() {
-
         var totalCalories=viewModel.totalCalories.value
         binding.caloriesTextView.text="Today's Calories: ${viewModel.todaysCalories.value}/$totalCalories"
-
-
     }
 
+    //displaying the recyclerview and calling the recycler view adapter
     fun display(it: ArrayList<DietViewModel.DietRecord>) {
-
         rView.adapter = DietDisplayAdapter(it,email)
         rView.layoutManager = LinearLayoutManager(this.requireContext())
         rView.setHasFixedSize(true)
     }
-
-
-
-
-
-
-
-
 
 }
